8c8b2d55f3f99f18b593995b5139c78ffe19c125
wizardabhi77 <vemulapalliabhinav@gmail.com>
2026-03-24 13:33:08 +0530

    add handleFollow and handleUnFollow functions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,9 +13,7 @@ export default function Home() {
 
     const navigate = useNavigate();
 
-    useEffect(()=> {
-
-        async function getUSer () {
+    async function getUSer () {
             
             const res = await fetch("http://localhost:5050/user",{
                 method: "GET",
@@ -28,11 +26,11 @@ export default function Home() {
             const user = await res.json();
 
             setUser(user);
-        }
 
-        getUSer();
+            console.log(user);
+    }
 
-        async function getFriends () {
+    async function getFriends () {
 
             const res = await fetch("http://localhost:5050/friends",{
                 method: "GET",
@@ -47,13 +45,71 @@ export default function Home() {
             setFriendsList(friends);
         }
 
-        getFriends();
-    }, [token])
+    useEffect(()=> {
 
-    async function handleProfile() {
-
-        navigate("/profile");
         
+
+        getUSer();
+
+        
+
+        getFriends();
+    }, [])
+
+    async function handleLogout() {
+
+        localStorage.removeItem("token");
+        navigate("/");
+    }
+
+    async function handleFollow(fid) {
+
+        const res = await fetch("http://localhost:5050/follow", {
+            method : "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization : "Bearer " + token 
+            },
+            body: JSON.stringify({
+                followId: fid
+            })
+        });
+
+        const data = await res.json();
+
+        if(!res.ok){
+            console.log("FETCH FAILED");
+            return;
+        }
+
+      
+
+        getUSer();
+        getFriends();
+    }
+
+    async function handleUnFollow(fid) {
+
+        const res = await fetch("http://localhost:5050/unfollow", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer " + token
+            },
+            body : JSON.stringify({
+                followId : fid
+            })       
+        });
+
+        const data = await res.json();
+
+        if(!res.ok){
+            console.log("unfollow Failed");
+            return;
+        }
+
+        getUSer();
+        getFriends();
     }
 
     if(!user) return <h1>LOADING...</h1>
@@ -64,7 +120,7 @@ export default function Home() {
 
             <h2>WELCOME {user.username}</h2>
 
-            <button onClick={handleProfile}>PROFILE</button> <br /> <br />
+            <button onClick={()=> navigate("/profile")}>PROFILE</button> <br /> <br />
 
             <h2>FRIENDS U NEED TO MAKE</h2>
             <ul>
@@ -72,11 +128,37 @@ export default function Home() {
                     return (
                         <li key={friend.id}>
                             <h3>{friend.username}</h3>
-                            <button>MAKE FRIEND</button>
+                            <button onClick={()=> handleFollow(friend.id)}>FOLLOW</button>
                         </li>
                     )
                 })}
             </ul>
+
+            <h2>FOLLOWERS</h2>
+            <ul>
+                {user.followers?.map((f)=> {
+                    return (
+                        <li key={f.follower.id}>
+                            <h3>{f.follower.username}</h3>
+                            
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <h2>FOLLOWING</h2>
+            <ul>
+                {user.following?.map((f) => {
+                    return (
+                        <li key={f.following.id}>
+                            <h3>{f.following.username}</h3>
+                            <button onClick={()=> handleUnFollow(f.following.id)}>UNFOLLOW</button>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <button onClick={handleLogout}>LOGOUT</button>
         </div>
     )
 }  
